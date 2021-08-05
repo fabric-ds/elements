8a777e188208f50534b16dabe2a97bbe54866eb4
Benjamin Akar <benjaminakar2001@gmail.com>
2021-08-05 14:38:11 +0200

    Refactor

---
+++ packages/breadcrumbs/src/fabric-breadcrumbs.js
@@ -0,0 +1,11 @@
+import { FabricWebComponent } from '../../utils';
+
+export class FabricBreadcrumbs extends FabricWebComponent {
+    connectedCallback() {
+        const children = Array.from(this.children)
+            .map((child) => child.outerHTML)
+            .join('<span class="select-none" aria-hidden>/</span>');
+
+        this.shadowRoot.innerHTML += `<nav class="flex space-x-8">${children}</nav>`;
+    }
+}
--- pages/includes/scripts.js
+++ pages/includes/scripts.js
@@ -1,5 +1,7 @@
-import { FabricModal } from "../../packages/modal/src/fabric-modal.js";
-import { FabricBox } from "../../packages/box/src/fabric-box.js";
+import { FabricModal } from '../../packages/modal/src/fabric-modal.js';
+import { FabricBox } from '../../packages/box/src/fabric-box.js';
+import { FabricBreadcrumbs } from '../../packages/breadcrumbs/src/fabric-breadcrumbs.js';
 
-customElements.define('fabric-modal', FabricModal);
-customElements.define('fabric-box', FabricBox);
+customElements.define('f-modal', FabricModal);
+customElements.define('f-box', FabricBox);
+customElements.define('f-breadcrumbs', FabricBreadcrumbs);
