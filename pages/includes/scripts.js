c4662531bb11bf146f641cd3ec7e281117a7070f
Benjamin Akar <benjaminakar2001@gmail.com>
2021-08-30 11:04:18 +0200

    Remove example code

--- pages/includes/scripts.js
+++ pages/includes/scripts.js
@@ -1,21 +1,6 @@
 import { FabricBox } from '../../packages/box/src/fabric-box.js';
 import { FabricBreadcrumbs } from '../../packages/breadcrumbs/src/fabric-breadcrumbs.js';
 import { FabricModal } from '../../packages/modal/src/fabric-modal.js';
-import { toast, updateToast, removeToast } from '../../packages/toast/src';
-
-const t = toast('hi', {
-    duration: 0,
-});
-
-setTimeout(() => {
-    updateToast(t.id, {
-        type: 'warning',
-    });
-}, 2500);
-
-setTimeout(() => {
-    removeToast(t.id);
-}, 4000);
 
 customElements.define('f-modal', FabricModal);
 customElements.define('f-box', FabricBox);
