1fde5efa285e1a8f8ee6fd3c74beefad76d1753d
tor-martin-storsletten <tor-martin.storsletten@finn.no>
2021-09-08 10:37:03 +0200

    fix: Import toast in scripts.js

--- pages/includes/scripts.js
+++ pages/includes/scripts.js
@@ -1,6 +1,7 @@
 import { FabricBox } from '../../packages/box/src/fabric-box.js';
 import { FabricBreadcrumbs } from '../../packages/breadcrumbs/src/fabric-breadcrumbs.js';
 import { FabricModal } from '../../packages/modal/src/fabric-modal.js';
+import { toast, removeToast, updateToast } from '../../packages/toast/src/index.js';
 
 customElements.define('f-modal', FabricModal);
 customElements.define('f-box', FabricBox);
