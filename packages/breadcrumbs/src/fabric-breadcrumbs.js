7c6a88b113cc2784dcda22cc82cae33af763e399
tor-martin-storsletten <tor-martin.storsletten@finn.no>
2021-09-13 09:38:56 +0200

    fix(breadcrumbs): aria-hidden on separator
    aria-hidden must be explicitly "true" for it to have the desired effect.

--- packages/breadcrumbs/src/fabric-breadcrumbs.js
+++ packages/breadcrumbs/src/fabric-breadcrumbs.js
@@ -4,7 +4,7 @@ export class FabricBreadcrumbs extends FabricWebComponent {
     connectedCallback() {
         const children = Array.from(this.children)
             .map((child) => child.outerHTML)
-            .join('<span class="select-none" aria-hidden>/</span>');
+            .join('<span class="select-none" aria-hidden="true">/</span>');
 
         this.shadowRoot.innerHTML += `<nav class="flex space-x-8">${children}</nav>`;
     }
