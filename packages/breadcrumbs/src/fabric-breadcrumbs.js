f272e14ba228bf0a3664e68ba4abf7951216ab79
tor-martin-storsletten <tor-martin.storsletten@finn.no>
2021-09-13 09:45:39 +0200

    fix(breadcrumbs): Add label and heading

--- packages/breadcrumbs/src/fabric-breadcrumbs.js
+++ packages/breadcrumbs/src/fabric-breadcrumbs.js
@@ -6,6 +6,12 @@ export class FabricBreadcrumbs extends FabricWebComponent {
             .map((child) => child.outerHTML)
             .join('<span class="select-none" aria-hidden="true">/</span>');
 
-        this.shadowRoot.innerHTML += `<nav class="flex space-x-8">${children}</nav>`;
+        this.shadowRoot.innerHTML += `<nav
+            aria-label="Her er du"
+            class="flex space-x-8"
+        >
+            <h2 class="sr-only">Her er du</h2>
+            ${children}
+        </nav>`;
     }
 }
