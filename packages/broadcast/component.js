81d0c68f5f46392051eb1a28100a853e8f7e8a46
Richard Walker <digitalsadhu@gmail.com>
2022-01-24 13:53:00 +0100

    feat: convert broadcast to lit element

--- packages/broadcast/component.js
+++ packages/broadcast/component.js
@@ -1,46 +1,63 @@
-import { FabricWebComponent, windowExists } from '../utils';
+import { LitElement, css, html } from 'lit-element';
+import { windowExists } from '../utils';
 
-export class FabricBroadcast extends FabricWebComponent {
+export class FabricBroadcast extends LitElement {
+  static styles = css`
+    <style>:host { display: block; }</style>
+    <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://assets.finn.no/pkg/@fabric-ds/css/v1/fabric.min.css"
+    />
+  `;
+
+  static properties = {
+    messages: {
+      type: Array,
+      attribute: true,
+      hasChanged(newVal, oldVal) {
+        if (!oldVal) return true;
+        const newIds = newVal.map(({ id }) => id).sort();
+        const oldIds = oldVal.map(({ id }) => id).sort();
+        return JSON.stringify(newIds) !== JSON.stringify(oldIds);
+      },
+    },
+    interval: { type: Number, attribute: true },
+    dev: { type: Boolean, attribute: true },
+    url: { type: String, attribute: true },
+    api: { type: String, attribute: true },
+  };
+
+  constructor() {
+    super();
+    this.messages = [];
+    this.interval = 30000;
+    this.dev = false;
+    this.url = windowExists ? window.location.href : '';
+    this.api = 'https://www.finn.no/broadcasts';
+  }
+
   async connectedCallback() {
-    const REFETCH_INTERVAL = Number(this.getAttribute('interval')) || 300000; // 300 000 ms = 5 minutes
-
-    // Fetch broadcast on load and setup refetch
-    await this.fetchMessage();
-    setInterval(async () => await this.fetchMessage(), REFETCH_INTERVAL);
+    super.connectedCallback();
+    if (windowExists) {
+      await this.fetchMessage();
+      setInterval(() => this.fetchMessage(), this.interval);
+    }
   }
 
   async fetchMessage() {
-    if (!windowExists) return;
-    const existing = this.shadowRoot.querySelector('#broadcast-toast');
-
-    // Generate url
-    const dev = Boolean(this.getAttribute('dev'));
-    const url = `https://${dev ? 'dev' : 'www'}.finn.no/broadcasts?path=${
-      this.getAttribute('url') || window.location.href
-    }`;
-
-    // Fetch message
+    const url = `${this.api}?path=${this.url}`;
     const res = await (await fetch(url)).json();
+    this.messages = res.length ? res : [];
+  }
 
-    // If response exists
-    if (res.length) {
-      this.message = res[0].message;
+  close(id) {
+    this.messages = this.messages.filter((message) => message.id !== id);
+  }
 
-      if (existing) {
-        // Container exists, update toast message
-        existing.setAttribute('text', this.message);
-      } else {
-        // Setup container with toast
-        this.shadowRoot.innerHTML += `
-          <div id="broadcast">
-            <f-toast id="broadcast-toast" type="warning" text="${this.message}"></f-toast>
-          </div>
-        `;
-      }
-    } else {
-      // No broadcast in response, remove container if broadcast previously existed
-      this.message = '';
-      if (existing) existing.remove();
-    }
+  render() {
+    return html`<div id="broadcast">
+      ${this.messages.map(m => html`<f-toast id="${m.id}" type="warning" text="${m.message}"></f-toast>`)}
+    </div>`;
   }
 }
