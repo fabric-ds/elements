422c2d5107f4fd459650d0c7ffcd25ff3cfc1e51
Runar Botten <runar.botten@finn.no>
2022-02-17 15:35:27 +0100

    Fix dev attribute. Issue #27

--- packages/broadcast/component.js
+++ packages/broadcast/component.js
@@ -14,7 +14,7 @@ export class FabricBroadcast extends FabricWebComponent {
     const existing = this.shadowRoot.querySelector('#broadcast-toast');
 
     // Generate url
-    const dev = Boolean(this.getAttribute('dev'));
+    const dev = this.getAttribute('dev') === 'true';
     const url = `https://${dev ? 'dev' : 'www'}.finn.no/broadcasts?path=${
       this.getAttribute('url') || window.location.href
     }`;
