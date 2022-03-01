f074bfd4f4cf0d2e789080e17fdaf9842ca51dd0
Richard Walker <digitalsadhu@gmail.com>
2022-03-01 12:40:16 +0100

    feat: refactor broadcast to give it independence from toast

--- packages/broadcast/component.js
+++ packages/broadcast/component.js
@@ -1,6 +1,6 @@
-import { LitElement } from 'lit-element';
+import { LitElement, html } from 'lit';
+import { repeat } from 'lit/directives/repeat.js';
 import { windowExists } from '../utils';
-import { FabricToastContainer } from '../..';
 
 export class FabricBroadcast extends LitElement {
   static properties = {
@@ -23,7 +23,6 @@ export class FabricBroadcast extends LitElement {
     this._messages = [];
     this.interval = 30000;
     this.url = windowExists ? window.location.href : '';
-    this._toastContainer = FabricToastContainer.init();
   }
 
   async connectedCallback() {
@@ -33,24 +32,57 @@ export class FabricBroadcast extends LitElement {
       return;
     }
     if (windowExists) {
-      await this.fetchMessage();
-      setInterval(() => this.fetchMessage(), this.interval);
+      await this._fetchMessage();
+      setInterval(() => this._fetchMessage(), this.interval);
     }
   }
 
-  async fetchMessage() {
+  async _fetchMessage() {
     const url = `${this.api}?path=${this.url}`;
     try {
       const res = await (await fetch(url)).json();
       this._messages = res.length ? res : [];
     } catch (err) {
-      console.error(`failed to fetch broadcasts from given url (${url})`)
+      console.error(`failed to fetch broadcasts from given url (${url})`);
     }
   }
 
+  get _broadcasts() {
+    if (!windowExists) return this._messages;
+    const ignoredBroadcasts = JSON.parse(window.localStorage.getItem('ignored-broadcasts')) || [];
+    return this._messages.filter(({ id }) => !ignoredBroadcasts.includes(id));
+  }
+
+  async _del(id) {
+    const el = this.renderRoot.querySelector(`#${id}`);
+    await el.collapse();
+    const ignoredBroadcasts = JSON.parse(window.localStorage.getItem('ignored-broadcasts')) || [];
+    ignoredBroadcasts.push(id.split('-')[1]);
+    window.localStorage.setItem('ignored-broadcasts', JSON.stringify(ignoredBroadcasts));
+  }
+
   render() {
-    for (const { id, message: text } of this._messages) {
-      this._toastContainer.set({ id: `broadcast-${id}`, text, type: 'warning', canclose: true });
-    }
+    return html`
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://assets.finn.no/pkg/@fabric-ds/css/v1/fabric.min.css"
+      />
+      <aside>
+        ${repeat(
+          this._broadcasts,
+          ({ id }) => `broadcast-${id}`,
+          ({ id, message }) => html`<f-toast
+            class="w-full"
+            id="broadcast-${id}"
+            type="warning"
+            text="${message}"
+            canclose
+            @close=${() => this._del(`broadcast-${id}`)}
+          >
+          </f-toast>`,
+        )}
+      </aside>
+    `;
   }
 }
