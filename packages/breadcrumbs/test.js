13f01860f28a121d3e3e0246fa752e37175e1abc
Richard Walker <digitalsadhu@gmail.com>
2022-03-09 12:33:50 +0100

    test: replace test setup with tap and playwright

--- packages/breadcrumbs/test.js
+++ packages/breadcrumbs/test.js
@@ -1,54 +1,79 @@
 /* eslint-disable no-undef */
-import { html, fixture, assert } from '@open-wc/testing';
-import '../../dist/index.js';
+import { test, teardown } from 'tap';
+import { Server } from './server.js';
+import { chromium } from 'playwright';
 
-const test = it;
+const server = new Server({ chromium });
+await server.start();
 
-test('Breadcrumb component renders on the page', async () => {
+teardown(() => server.stop());
+
+test('Breadcrumb component renders on the page', async (t) => {
   // GIVEN: A component with 1 breadcrumb
-  const component = html`<f-breadcrumbs
-    ><a href="#/url/1">Eiendom</a></f-breadcrumbs
-  >`;
+  const component = `
+    <f-breadcrumbs>
+      <a href="#/url/1">Eiendom</a>
+    </f-breadcrumbs>
+  `;
 
   // WHEN: the component is added to the page
-  const el = await fixture(component);
+  const page = await server.fixture(component);
 
   // THEN: the component is visible in the DOM
-  assert.match(el, 'f-breadcrumbs');
-  assert.equal(el.querySelector('nav').children.length, 2);
+  t.equal(await page.innerText('nav a'), 'Eiendom');
 });
 
-test('Breadcrumb component interleaves / characters between breadcrumb items', async () => {
+test('Breadcrumb component interleaves / characters between breadcrumb items', async (t) => {
   // GIVEN: A component with 2 breadcrumbs
-  const component = html`<f-breadcrumbs>
-    <a href="#/url/1">Eiendom</a>
-    <a href="#/url/2">Torget</a>
-  </f-breadcrumbs>`;
+  const component = `
+    <f-breadcrumbs>
+        <a href="#/url/1">Eiendom</a>
+        <a href="#/url/2">Torget</a>
+    </f-breadcrumbs>
+  `;
 
   // WHEN: the component is added to the page AND spans are selected
-  const el = await fixture(component);
-  const dividers = el.querySelectorAll('span');
-
+  const page = await server.fixture(component);
+  
   // THEN: a single divider should have been interleaved with the breadcrumbs
-  assert.equal(dividers.length, 1);
-  assert.equal(dividers[0].innerText, '/');
+  t.equal(await page.innerText('f-breadcrumbs span'), '/');
 });
 
-test('Breadcrumb component includes child elements as given', async () => {
+test('Breadcrumb component with anchor child elements', async (t) => {
   // GIVEN: A component with 3 breadcrumbs
-  const component = html`<f-breadcrumbs>
-    <a href="#/url/1">Eiendom</a>
-    <a href="#/url/2">Torget</a>
-    <a href="#/url/3">Oslo</a>
-  </f-breadcrumbs>`;
+  const component = `
+    <f-breadcrumbs>
+      <a href="#/url/1">Eiendom</a>
+      <a href="#/url/2">Torget</a>
+      <a href="#/url/3">Oslo</a>
+    </f-breadcrumbs>
+  `;
 
   // WHEN: the component is added to the page AND a elements are selected
-  const el = await fixture(component);
-  const breadcrumbs = el.querySelectorAll('a');
+  const page = await server.fixture(component);
 
   // THEN: there should be three breadcrumbs in the DOM
-  assert.equal(breadcrumbs.length, 3);
-  assert.equal(breadcrumbs[0].innerText, 'Eiendom');
-  assert.equal(breadcrumbs[1].innerText, 'Torget');
-  assert.equal(breadcrumbs[2].innerText, 'Oslo');
+  t.equal(await page.locator('f-breadcrumbs a').count(), 3);
+  t.equal(await page.locator('f-breadcrumbs span').count(), 2);
+  t.match(await page.innerText(':nth-match(f-breadcrumbs a, 1)'), 'Eiendom');
+  t.match(await page.innerText(':nth-match(f-breadcrumbs a, 2)'), 'Torget');
+  t.match(await page.innerText(':nth-match(f-breadcrumbs a, 3)'), 'Oslo');
+});
+
+test('Breadcrumb component with last element as a span', async (t) => {
+  // GIVEN: A component with 3 breadcrumbs
+  const component = `
+    <f-breadcrumbs>
+      <a href="#/url/1">Eiendom</a>
+      <a href="#/url/2">Torget</a>
+      <span aria-current="page">Oslo</span>
+    </f-breadcrumbs>
+  `;
+
+  // WHEN: the component is added to the page AND a elements are selected
+  const page = await server.fixture(component);
+
+  // THEN: there should be three breadcrumbs in the DOM
+  t.equal(await page.locator('f-breadcrumbs a').count(), 2);
+  t.equal(await page.locator('f-breadcrumbs span').count(), 3);
 });
