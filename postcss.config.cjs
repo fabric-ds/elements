97354f170b345ec8cd97d8e183d747fe930eea1d
Richard Walker <digitalsadhu@gmail.com>
2022-04-25 14:33:47 +0200

    build: fix broken build

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,19 +1,19 @@
 /* eslint-env node */
-const path = require('path');
-const tailwindConfig = require('./tailwind.config');
+// const path = require('path');
+const tailwindConfig = require('./tailwind.config.cjs');
 
 // PostCSS setup to use Tailwind classes with lit-element
 // Inspired by https://dev.to/43081j/using-tailwind-at-build-time-with-web-components-1bhm
 
 module.exports = (ctx) => {
-    const filePath = path.join(ctx.file.dirname, ctx.file.basename);
+    // const filePath = path.join(ctx.file.dirname, ctx.file.basename);
 
     const plugins = [
         require('tailwindcss')({
             ...tailwindConfig,
             // Always purge. So we don't add all of Tailwind in our custom elements.
             // We do this on a per file basis, as we need to include the utilities in the shadow dom
-            purge: { enabled: true, content: [filePath] },
+            // purge: { enabled: true, content: [filePath] },
         }),
         require('postcss-discard-comments')(),
         require('postcss-discard-empty')(),
@@ -25,7 +25,7 @@ module.exports = (ctx) => {
     }
 
     return {
-        syntax: require('@stylelint/postcss-css-in-js'),
+        // syntax: require('@stylelint/postcss-css-in-js'),
         plugins,
     };
 };
