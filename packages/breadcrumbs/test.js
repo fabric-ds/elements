549e43e82c8b76cefce5adcac43b0c9bf67d086a
Richard Walker <digitalsadhu@gmail.com>
2022-04-26 12:12:53 +0200

    test: improve playwright/tap breadcrumb tests

--- packages/breadcrumbs/test.js
+++ packages/breadcrumbs/test.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-undef */
-import { test, teardown } from 'tap';
-import { Server } from './server.js';
+import tap, { test, beforeEach, teardown } from 'tap';
 import { chromium } from 'playwright';
 
-const server = new Server({ chromium });
-await server.start();
+tap.before(async () => {
+  const browser = await chromium.launch({ headless: true });
+  tap.context.browser = browser;
+});
 
-teardown(() => server.stop());
+beforeEach(async (t) => {
+  const { browser } = t.context;
+  const context = await browser.newContext();
+  // context.setDefaultTimeout(2000);
+  t.context.page = await context.newPage();
+});
+
+teardown(async () => {
+  const { browser } = tap.context;
+  browser.close();
+});
 
 test('Breadcrumb component renders on the page', async (t) => {
   // GIVEN: A component with 1 breadcrumb
@@ -17,10 +28,12 @@ test('Breadcrumb component renders on the page', async (t) => {
   `;
 
   // WHEN: the component is added to the page
-  const page = await server.fixture(component);
+  const { page } = t.context;
+  await page.setContent(component);
+  await page.addScriptTag({ path: './dist/index.js', type: 'module' });
 
   // THEN: the component is visible in the DOM
-  t.equal(await page.innerText('nav a'), 'Eiendom');
+  t.equal(await page.innerText('nav a'), 'Eiendom', 'An Eiendom a tag should be added to the page');
 });
 
 test('Breadcrumb component interleaves / characters between breadcrumb items', async (t) => {
@@ -33,10 +46,12 @@ test('Breadcrumb component interleaves / characters between breadcrumb items', a
   `;
 
   // WHEN: the component is added to the page AND spans are selected
-  const page = await server.fixture(component);
+  const { page } = t.context;
+  await page.setContent(component);
+  await page.addScriptTag({ path: './dist/index.js', type: 'module' });
   
   // THEN: a single divider should have been interleaved with the breadcrumbs
-  t.equal(await page.innerText('f-breadcrumbs span'), '/');
+  t.equal(await page.innerText('f-breadcrumbs span'), '/', 'Divider slashes should be added');
 });
 
 test('Breadcrumb component with anchor child elements', async (t) => {
@@ -50,14 +65,16 @@ test('Breadcrumb component with anchor child elements', async (t) => {
   `;
 
   // WHEN: the component is added to the page AND a elements are selected
-  const page = await server.fixture(component);
+  const { page } = t.context;
+  await page.setContent(component);
+  await page.addScriptTag({ path: './dist/index.js', type: 'module' });
 
   // THEN: there should be three breadcrumbs in the DOM
-  t.equal(await page.locator('f-breadcrumbs a').count(), 3);
-  t.equal(await page.locator('f-breadcrumbs span').count(), 2);
-  t.match(await page.innerText(':nth-match(f-breadcrumbs a, 1)'), 'Eiendom');
-  t.match(await page.innerText(':nth-match(f-breadcrumbs a, 2)'), 'Torget');
-  t.match(await page.innerText(':nth-match(f-breadcrumbs a, 3)'), 'Oslo');
+  t.equal(await page.locator('f-breadcrumbs a').count(), 3, '3 a tags should be present');
+  t.equal(await page.locator('f-breadcrumbs span').count(), 2, '2 span tags should be present');
+  t.match(await page.innerText(':nth-match(f-breadcrumbs a, 1)'), 'Eiendom', 'The first segment should be Eiendom');
+  t.match(await page.innerText(':nth-match(f-breadcrumbs a, 2)'), 'Torget', 'The second segment should be Torget');
+  t.match(await page.innerText(':nth-match(f-breadcrumbs a, 3)'), 'Oslo', 'The third segment should be Oslo');
 });
 
 test('Breadcrumb component with last element as a span', async (t) => {
@@ -71,9 +88,11 @@ test('Breadcrumb component with last element as a span', async (t) => {
   `;
 
   // WHEN: the component is added to the page AND a elements are selected
-  const page = await server.fixture(component);
+  const { page } = t.context;
+  await page.setContent(component);
+  await page.addScriptTag({ path: './dist/index.js', type: 'module' });
 
   // THEN: there should be three breadcrumbs in the DOM
-  t.equal(await page.locator('f-breadcrumbs a').count(), 2);
-  t.equal(await page.locator('f-breadcrumbs span').count(), 3);
+  t.equal(await page.locator('f-breadcrumbs a').count(), 2, '2 child a tags should be present');
+  t.equal(await page.locator('f-breadcrumbs span').count(), 3, '3 child span tags should be present');
 });
