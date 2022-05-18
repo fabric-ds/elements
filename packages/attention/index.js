1e8c2d83538dc1c052d1490f07339466a5640aab
Richard Walker <digitalsadhu@gmail.com>
2022-05-18 15:21:44 +0200

    --wip-- [skip ci]

--- packages/attention/index.js
+++ packages/attention/index.js
@@ -1,6 +1,7 @@
 import { html, css } from 'lit';
-import { ifDefined } from 'lit/directives/if-defined.js';
+// import { ifDefined } from 'lit/directives/if-defined.js';
 import { attention as a } from '@fabric-ds/css/component-classes';
+import { useRecompute, directions } from '@fabric-ds/core/attention';
 import { FabricElement, fclasses } from '../utils';
 
 class FabricAttention extends FabricElement {
@@ -15,12 +16,50 @@ class FabricAttention extends FabricElement {
     callout: { type: Boolean },
     popover: { type: Boolean },
     noArrow: { type: Boolean },
-    attentionClass: {},
-    actualDirection: {},
+    show: { type: Boolean },
+    top: { type: Boolean },
+    right: { type: Boolean },
+    bottom: { type: Boolean },
+    left: { type: Boolean },
+    attentionClass: { type: String },
+    actualDirection: { type: String },
+
   };
 
   constructor() {
     super();
+
+    this.top = false;
+    this.right = false;
+    this.bottom = false;
+    this.left = false;
+
+    this.attentionState = {
+      isShowing: this.show,
+      isCallout: this.callout,
+      actualDirection: this.actualDirection,
+      get directionName() {
+        return directions.find(e => this[e]);
+      },
+      get arrowEl() { 
+        return this.renderRoot.querySelector('f-attention-arrow') || null;
+      },
+      attentionEl: this,
+      get targetEl() {
+        return document.querySelector(this.targetEl) || null;
+      },
+      top: this.top,
+      right: this.right,
+      bottom: this.bottom,
+      left: this.left,
+      tooltip: this.tooltip,
+      popover: this.popover,
+      callout: this.callout,
+      noArrow: this.noArrow,
+      waitForDOM: () => new Promise(resolve => {
+        resolve();
+      }),
+    };
   }
 
   get _wrapperClasses() {
@@ -48,6 +87,17 @@ class FabricAttention extends FabricElement {
         </div>
       </div>
     `;
+  }
+
+  updated(changedProperties) {
+    const top = changedProperties.get('top');
+    const right = changedProperties.get('right');
+    const bottom = changedProperties.get('bottom');
+    const left = changedProperties.get('left');
+    const targetEl = changedProperties.get('targetEl');
+    if (top || right || bottom || left || targetEl) {
+      useRecompute(this.attentionState);
+    }
   }
 }
 
