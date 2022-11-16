f77f081eb40d9f811d5fbcc6060c33996450d0cd
Richard Walker <digitalsadhu@gmail.com>
2022-11-16 21:53:00 +1300

    feat: add build script that produces constructed stylesheet

---
+++ build-elements.js
@@ -0,0 +1,55 @@
+// @ts-nocheck
+
+import fs from 'fs';
+import path from 'path';
+import postcss from 'postcss';
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+import atImport from 'postcss-import';
+import presetEnv from 'postcss-preset-env';
+import importSvg from 'postcss-import-svg';
+import { breakpoints as customMedia } from '@fabric-ds/css/colors.js';
+import tailwindConfig from '@fabric-ds/tailwind-config';
+import { createRequire } from 'module';
+import { classes } from '@fabric-ds/css/component-classes/list.js';
+
+const require = createRequire(import.meta.url); // have to do these hacks it seems because import.meta.resolve is cranky
+const iconsLocation = path.resolve(path.dirname(require.resolve('@fabric-ds/icons/package.json')), 'dist');
+
+const from = './elements.css';
+const to = './dist/elements.min.js';
+const css = fs.readFileSync(from, 'utf-8');
+const plugins = [
+    atImport,
+    tailwind({
+        ...tailwindConfig,
+        purge: {
+            enabled: true,
+            content: ['./packages/**/*.js'],
+            safelist: classes,
+        },
+    }),
+    presetEnv({
+        stage: 0,
+        browsers: 'extends @finn-no/browserslist-config',
+        importFrom: { customMedia },
+        features: {
+            'focus-visible-pseudo-class': false,
+        },
+    }),
+    importSvg({
+        paths: [iconsLocation],
+    }),
+    autoprefixer,
+    cssnano({ preset: 'default' }),
+];
+const result = await postcss(plugins).process(css, { from, to });
+const replaced = result.css.replaceAll('\\:', '\\\\:');
+const constructedStylesheet = `import {css} from 'lit';
+export const styles = css\`
+${replaced}
+\`;
+`;
+fs.mkdirSync('./dist', { recursive: true });
+fs.writeFileSync(to, constructedStylesheet, { encoding: 'utf-8' });
